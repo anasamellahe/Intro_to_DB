bbc065f6e07f8da138a16f5e20536fbbfdd477e6
Anamella <amellaheanas@gmail.com>
2025-06-27 12:35:30 -0400

    1. Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,25 +1,64 @@
 import mysql.connector
 
-dataBaseScript = "alx_book_store.sql"
-host = "localhost"
-user = "root"
-password = "26627777"
+scriptFileName = "alx_book_store.sql"
+host = "your host"
+user = "your user"
+password = "your Password"
 
-try:
-    with mysql.connector.connect(host=host, user=user, password=password) as myDataBase:
-        pass
-except:
-    print("connection fail")
-else:
-    print("connection success \n")
-try:
-    with open(dataBaseScript, "r") as sqlScript:
-        pass
-except:
-    print(f"can't open the file {dataBaseScript}")
-else:
-    print(f"file {dataBaseScript} open successfully")
+def connectToDB(user, host, password):
+    try:
+        myConnector = mysql.connector.connect(user=user, host=host, password=password)
+    except Exception as e:
+        print("exception here", e)
+        return None
+    else:
+        print("connection success \n")
+    return myConnector
 
-# myCursor = myDataBase.cursor()
-print(sqlScript.read())
 
+def openScript(scriptFileName):
+    try:
+        with open(scriptFileName, 'r') as myScript:
+            scriptContent = myScript.read()
+    except Exception as e:
+        print("exception here", e)
+        return None
+    else:
+        return scriptContent
+
+def splitSQLScript(mySQLScript):
+    myList = mySQLScript.split(sep=";")
+    for index in range(len(myList)):
+        myList[index] = myList[index].replace("\n", "").strip()
+        if len(myList[index]) == 0:
+            myList.pop(index)
+    return myList;
+        
+def executeCommands(command, connecterOBJ ,cursorOBJ):
+    try:
+        cursorOBJ.execute(command)
+    except Exception as e:
+        print(f"Failed to execute:\n{command}\nError: {e}\n")
+        return False
+    else:
+        print(f"Executed: {command[:50]}...")
+        return True
+
+def startConnection():
+    connectorOBJ  = connectToDB(user=user, host=host, password=password)
+    if (connectorOBJ == None):
+        exit(1)
+    mySQLScript = openScript(scriptFileName=scriptFileName)
+    if mySQLScript == None:
+        connectorOBJ.close()
+        exit(1)
+    commands = splitSQLScript(mySQLScript)
+    cursorOBJ = connectorOBJ.cursor()
+    for command in commands:
+        executeCommands(command=command, connecterOBJ=connectorOBJ, cursorOBJ=cursorOBJ)
+    connectorOBJ.commit()
+    connectorOBJ.close()
+    print("Database 'alx_book_store' created successfully!.")
+
+if __name__ == "__main__":
+    startConnection()
