ae738547f7e3356f54325102ea9911160467fc51
Anamella <amellaheanas@gmail.com>
2025-06-27 13:44:43 -0400

    fix Order_Details FOREIGN KEY (order_id) REFERENCES Orders(order_id) and     FOREIGN KEY (book_id) REFERENCES Books(book_id)

--- task_2.sql
+++ task_2.sql
@@ -36,4 +36,8 @@ CREATE TABLE IF NOT EXISTS Order_Details(
     order_id INT, 
     book_id INT,
     quantity DOUBLE
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+
 );
