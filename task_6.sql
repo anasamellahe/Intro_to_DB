2e14dcc6ef35d334966d24eb5d719252c420cd40
Anamella <amellaheanas@gmail.com>
2025-06-28 09:46:20 -0400

    fix the address

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 use alx_book_store;
 INSERT INTO customer (customer_id, customer_name, email, address)  VALUES (1,  'Cole Baidoo', 'cbaidoo@sandtech.com', "123 Happiness Ave.");
-INSERT INTO customer (customer_id, customer_name, email, address)  VALUES (2,  'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness Ave.");
-INSERT INTO customer (customer_id, customer_name, email, address)  VALUES (3,  'Obed Ehoneah', 'eobed@sandtech.com', "125 Happiness Ave.");
-INSERT INTO customer (customer_id, customer_name, email, address)  VALUES (4,  'Nehemial Kamolu', 'nkamolu@sandtech.com', "126 Happiness Ave.");
+INSERT INTO customer (customer_id, customer_name, email, address)  VALUES (2,  'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness  Ave.");
+INSERT INTO customer (customer_id, customer_name, email, address)  VALUES (3,  'Obed Ehoneah', 'eobed@sandtech.com', "125 Happiness  Ave.");
+INSERT INTO customer (customer_id, customer_name, email, address)  VALUES (4,  'Nehemial Kamolu', 'nkamolu@sandtech.com', "126 Happiness  Ave.");
