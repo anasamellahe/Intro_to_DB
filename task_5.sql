edac84d8b8599aef83e0f469c6435f379664f73f
Anamella <amellaheanas@gmail.com>
2025-06-28 09:37:14 -0400

    fix insert

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
 use alx_book_store;
-INSERT INTO Customer (customer_id, customer_name, email, address)  VALUES (1,  'Cole Baidoo', 'cbaidoo@sandtech.com', 'cbaidoo@sandtech.com');
+INSERT INTO customer (customer_id, customer_name, email, address)  VALUES (1,  'Cole Baidoo', 'cbaidoo@sandtech.com', 'cbaidoo@sandtech.com');
