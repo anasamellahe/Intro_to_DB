75205f5429a9e108ad4b175ec096a6ad91513cf9
Anamella <amellaheanas@gmail.com>
2025-06-28 09:36:16 -0400

    change the table name to customer

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
 use alx_book_store;
-INSERT INTO Customers (customer_id, customer_name, email, address)  VALUES (1,  'Cole Baidoo', 'cbaidoo@sandtech.com', 'cbaidoo@sandtech.com');
+INSERT INTO Customer (customer_id, customer_name, email, address)  VALUES (1,  'Cole Baidoo', 'cbaidoo@sandtech.com', 'cbaidoo@sandtech.com');
